f2fcc207a8ad9135cbd0118fe5ceb4b35b9f3660
nicolaivalenti <nicolai.valenti@gmail.com>
2023-07-22 18:37:28 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,21 @@
+number = input("scrivi \n")
+print("hai scritto: \n" + number)
+
+
+class Circle():
+    def __init__(self, radius):
+        self.radius = radius
+
+    def areaclc(self):
+
+        area = self.radius * self.radius * 3.14
+        return(area)
+
+
+circle1 = Circle(int(number))
+print(circle1.areaclc())
+
+
+
+
+
